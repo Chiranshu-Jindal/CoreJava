0f3e66a662f22253ea46dc8d6767bb8912335764
chish <chish@Chiranshu>
2024-07-26 19:13:03 +0530

    git remote add origin https://github.com/Chiranshu-Jindal/Chiranshu-Jindal.git git branch -M main git push -u origin main

--- Core Java/src/com/project/HashMap/Mainclass.java
+++ Core Java/src/com/project/HashMap/Mainclass.java
@@ -1,49 +1,47 @@
 package com.project.HashMap;
 
-import java.util.Scanner;
 import java.util.HashMap;
-import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
 public class Mainclass {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		Populatedsocietydata data = new Populatedsocietydata();
-		HashMap<Integer, ArrayList<Person>> societydata = new HashMap<>();
-		societydata=data.populateData();
-			
-	    System.out.print("enter the room no.:-");
-		int userkey = input.nextInt();		
-			
-		if(societydata.containsKey(userkey)) {	
-			double totalincome=0;
-			ArrayList<Person> retrievedItem = societydata.get(userkey);
-			System.out.println("Total numbers of person in the appartment no.: "+ userkey + " are " + retrievedItem.size());
-			for(int i = 0 ; i< retrievedItem.size();i++)
-			{
+		HashMap<Integer, List<Person>> societydata = new HashMap<>();
+		societydata = data.populateData();
+		System.out.print("enter the room no.:-");
+		int userkey = input.nextInt();
+
+		if (societydata.containsKey(userkey)) {
+			double totalincome = 0;
+			List<Person> retrievedItem = societydata.get(userkey);
+			System.out.println(
+					"Total numbers of person in the appartment no." + userkey + ":- " + retrievedItem.size());
+			for (int i = 0; i < retrievedItem.size(); i++) {
 				Person p = retrievedItem.get(i);
 				Salarystructure s = p.getsalarystu();
-	            System.out.print("person "+ (i+1) +","+p.getName()+"," + p.getRelations()+","+p.getEmail()+","+p.getAge()+","+ p.getMobile());
-	            
-	            if(s!=null) {
-	            	System.out.print(","+s.getOccupation());
-	            	System.out.print(","+s.getsalary());
-	            	totalincome += s.getsalary();
-	            }
-	        System.out.println();	
-		  	}
-			System.out.println("total income of this appartment is "+totalincome);
-		}
-			else
-			{	
-				System.out.println("the room number is wrong");
+				System.out.print("person" + (i + 1)+" , ");
+				System.out.print("Name:- "+ p.getName()+" , ");
+				System.out.print("Relation:- "+p.getRelations()+" , ");
+				System.out.print("Email:-"+p.getEmail()+" , ");
+				System.out.print("Age:-"+p.getAge()+" , ");
+				System.out.print("Mobile:-"+p.getMobile()+" , ");
+
+				if (s != null) {
+					System.out.print("Occupation:-" + s.getOccupation()+" , ");
+					System.out.print("Salary: " + s.getsalary());
+					totalincome += s.getsalary();
+				}
+				System.out.println();
 			}
-			input.close();
-	        
-			
+			System.out.println("total income of this appartment is " + totalincome);
+		} else {
+			System.out.println("the room number is wrong");
 		}
+		input.close();
+
 	}
+}
--- Core Java/src/com/project/HashMap/Populatedsocietydata.java
+++ Core Java/src/com/project/HashMap/Populatedsocietydata.java
@@ -1,121 +1,57 @@
 package com.project.HashMap;
-import java.util.HashMap;
-import java.util.Scanner;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Populatedsocietydata {
-    HashMap<Integer, ArrayList<Person>> populateData() 
-      {
-       HashMap<Integer, ArrayList<Person>> map = new HashMap<>();
-       ArrayList<Person> list = new ArrayList<Person>();
-		try{
-			BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\chish\\OneDrive\\Desktop\\populatedsocietydata.csv"));
-			String read="";
-			while((read=reader.readLine())!= null) {
+	HashMap<Integer, List<Person>> populateData() {
+		HashMap<Integer, List<Person>> map = new HashMap<>();
+
+		try {
+			BufferedReader reader = new BufferedReader(
+					new FileReader("C:\\Users\\chish\\OneDrive\\Desktop\\populatedsocietydata.csv"));
+			String read = "";
+
+			while ((read = reader.readLine()) != null) {
 				String[] token = read.split(",");
-			   if(token.length>=9) {
-			    int appartmentnumber=Integer.parseInt(token[0]);
-			    String personnumber=token[1];
-			    String Name=token[2];
-			    String relation=token[3];
-			    String Email=token[4];
-			    int age=Integer.parseInt(token[5]);
-			    String mobile=token[6];
-			    String occupation=token[7];
-			    double salary=Double.parseDouble(token[8]);
-			        
-			    Salarystructure s1 = new Salarystructure();
-			    s1.setSalary(salary);
-			    s1.setOccupation(occupation);	
-			    
-			    Person p1=new Person();
-			    p1.setName(Name);
-			    p1.setMobile(mobile);
-			    p1.setEmail(Email);
-			    p1.setRelations(Relation.valueOf(relation.toUpperCase()));
-			    p1.setAge(age);
-			    p1.setsalarystu(s1);
-			    
-//			    map.computeIfAbsent(appartmentnumber, k -> new ArrayList<>()).add(p1);
-			    if (!map.containsKey(appartmentnumber)) {
-			        map.put(appartmentnumber, new ArrayList<>());
-			    }
-			        map.get(appartmentnumber).add(p1);
-			    
-			   }
-			   }
-		}
-		catch(IOException e) {
+
+				if (token.length>=6) {
+					int appartmentnumber = Integer.parseInt(token[0]);
+					String personnumber = token[1];
+					String Name = token[2];
+					String relation = token[3];
+					String Email = token[4];
+					int age = Integer.parseInt(token[5]);
+					String mobile = (token.length>=7)?token[6]:null;
+					String occupation = (token.length>=8)?token[7]:null;
+					double salary = (token.length>=9)?Double.parseDouble(token[8]):0.0;
+
+					Salarystructure s1 = new Salarystructure();
+					s1.setSalary(salary);
+					s1.setOccupation(occupation);
+
+					Person p1 = new Person();
+					p1.setName(Name);
+					p1.setMobile(mobile);
+					p1.setEmail(Email);
+					p1.setRelations(Relation.valueOf(relation.toUpperCase()));
+					p1.setAge(age);
+					p1.setsalarystu(s1);
+					
+					List<Person> appartmentresidents = map.getOrDefault(appartmentnumber, new ArrayList<>());
+
+					appartmentresidents.add(p1);
+					map.put(appartmentnumber,appartmentresidents);
+				}
+			}
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-	return map;
-      }
+
+		return map;
+	}
 }
-      
-//	Salarystructure s1 = new Salarystructure();
-//	s1.setSalary(20000);
-//	s1.setOccupation("software engineer");
-//	
-//	Salarystructure s2 = new Salarystructure();
-//	s2.setSalary(10000);
-//	s2.setOccupation("software engineer");
-//	
-//	Salarystructure s4 = new Salarystructure();
-//	s4.setSalary(25000);
-//	s4.setOccupation("accountant");
-//	
-//	Salarystructure s5 = new Salarystructure();
-//	s5.setOccupation("housewife");
-//	
-//	Person p1 = new Person();
-//	p1.setName("rohit");
-//    p1.setMobile("89xxxxxx83");
-//    p1.setEmail("rohit4@gmail.com");
-//    p1.setRelations(Relation.FATHER);
-//    p1.setAge(38);
-//    p1.setsalarystu(s1);
-//    
-//	Person p2 = new Person();
-//	p2.setName("pooja");
-//    p2.setMobile("93xxxxxx56");
-//    p2.setEmail("xyz@gmail.com");
-//    p2.setRelations(Relation.MOTHER);
-//    p2.setAge(33);
-//    p2.setsalarystu(s2);
-//    
-//    Person p3 = new Person();
-//    p3.setName("chirag");
-//    p3.setEmail("xx@gmail.com");
-//    p3.setRelations(Relation.SON);
-//    p3.setAge(10); 
-//       
-//    Person p4 = new Person();
-//    p4.setName("rahul");
-//    p4.setEmail("xx@yahoo.com");
-//    p4.setMobile("78xxxxxx90");
-//    p4.setRelations(Relation.HUSBAND);
-//    p4.setAge(25);
-//    p4.setsalarystu(s4);;
-//    
-//    Person p5 = new Person();
-//    p5.setName("rachita");
-//    p5.setEmail("xx@yahoo.com");
-//    p5.setMobile("67xxxxxx93");
-//    p5.setRelations(Relation.WIFE);
-//    p5.setsalarystu(s5);
-//    
-//    ArrayList<Person> appartment1 = new ArrayList<Person>();
-//    appartment1.add(p1);
-//    appartment1.add(p2);
-//    appartment1.add(p3);
-//        
-//    ArrayList<Person> appartment2 = new ArrayList<Person>();
-//    appartment2.add(p4);
-//    appartment2.add(p5);
-//    
-//    map.put(101,appartment1);
-//	map.put(102,appartment2);
-   
